ac8432847c13faaf9223c515ac6fd0986fd5f8c7
Zijie Jiang <xiaojiang990209@gmail.com>
2018-02-07 22:27:46 -0500

    Week of code 36

--- Week of Code 36/ways_to_give_a_check.cc
+++ Week of Code 36/ways_to_give_a_check.cc
@@ -4,7 +4,7 @@
 using namespace std;
 
 bool valid_position(int i, int j) {
-    return i >= 0 && i <= 8 && j >= 0 && j <= 8;
+    return i >= 0 && i < 8 && j >= 0 && j < 8;
 }
 
 
@@ -13,9 +13,9 @@ bool valid_position(int i, int j) {
 //         1: diagnoal
 //         2: both
 // we also have a black/white king param to enhance reusability
-// we have 0: see if white king is checked
-//         1: see if black king is checked
-bool check(int i, int j, vector<vector<char>> board, int mode, int king) {
+// we have 'k': represents black king being attacked
+//         'K': represents white king being attacked
+bool check(int i, int j, vector<vector<char>> board, int mode, char attacked_king) {
     for (int di = -1; di <= 1; di++) {
         for (int dj = -1; dj <= 1; dj++) {
             if (mode == 0 && di * dj != 0) continue;
@@ -24,15 +24,13 @@ bool check(int i, int j, vector<vector<char>> board, int mode, int king) {
             int new_i = i + di;
             int new_j = j + dj;
             while (valid_position(new_i, new_j)) {
-                if (board[new_i][new_j] == 'K' && king == 0) {
-                    return true;
-                } else if (board[new_i][new_j] == 'k' && king == 1) {
+                if (board[new_i][new_j] == attacked_king) {
                     return true;
                 } else if (board[new_i][new_j] == '#') {
                     new_i += di;
                     new_j += dj;
                 } else {
-                    return false;
+                    break;
                 }
             }
         }
@@ -40,60 +38,90 @@ bool check(int i, int j, vector<vector<char>> board, int mode, int king) {
     return false;
 }
 
-bool is_white_king_attacked(int i, int j, char piece, vector<vector<char>> board) {
-    switch(piece) {
-        case 'k': for (int di = -1; di <= 1; di++) {
-                    for (int dj = -1; dj <= 1; dj++) {
-                        if (di == 0 && dj == 0) continue;
-                        int new_i = i + di;
-                        int new_j = j + dj;
-                        if (valid_position(new_i, new_j) && board[new_i][new_j] == 'K'){
-                            return true;
-                        } else if (valid_position(new_i, new_j) && board[new_i][new_j] == '#') {
-                            continue;
-                        } else {
-                            return false;
-                        }
-                    }
-                  }
-                  return false;
-        case 'n': for (int di = -2; di <= 2; di++) {
-                      for (int dj = -2; dj <= 2; dj++) {
-                          if (di * dj != 2) continue;
-                          int new_i = i + di;
-                          int new_j = j + dj;
-                          if (valid_position(new_i, new_j) && board[new_i][new_j] == 'K') {
-                              return true;
-                          } else if (valid_position(new_i, new_j) && board[new_i][new_j] == '#') {
-                              continue;
-                          } else {
-                              return false;
-                          }
-                      }
-                  }
-                  return false;
-        case 'q': return check(i, j, board, 2, 0); 
-        case 'r': return check(i, j, board, 0, 0);
-        case 'b': return check(i, j, board, 1, 0);
+bool check_knight(int i, int j, vector<vector<char>> board, char attacked_king) {
+    for (int di = -2; di <= 2; di++) {
+        for (int dj = -2; dj <= 2; dj++) {
+            if (di * dj != 2 && di * dj != -2) continue;
+            int new_i = i + di;
+            int new_j = j + dj;
+            //cout << "new_i: " << new_i << " new_j: " << new_j << endl;
+            if (valid_position(new_i, new_j) && board[new_i][new_j] == attacked_king) {
+                return true;
+            } else {
+                continue;
+            }
+        }
+    }
+    return false;
+}
+
+bool check_king(int i, int j, vector<vector<char>> board, char attacked_king) {
+    for (int di = -1; di <= 1; di++) {
+        for (int dj = -1; dj <= 1; dj++) {
+            if (di == 0 && dj == 0) continue;
+            int new_i = i + di;
+            int new_j = j + dj;
+            if (valid_position(new_i, new_j) && board[new_i][new_j] == attacked_king){
+                return true;
+            } else {
+                continue;
+            }
+        }
+    }
+    return false;
+}
+
+bool is_king_attacked(int i, int j, char piece, vector<vector<char>> board) {
+    char k = piece >= 'a' ? 'k' : 'K';
+    char n = piece >= 'a' ? 'n' : 'N';
+    char q = piece >= 'a' ? 'q' : 'Q';
+    char r = piece >= 'a' ? 'r' : 'R';
+    char b = piece >= 'a' ? 'b' : 'B';
+    char attacked_king = piece >= 'a' ? 'K' : 'k';
+    if (piece == k) {
+        //cout << "fine here pt 1" << endl;
+        return check_king(i, j, board, attacked_king);
+    } else if (piece == n) {
+        //cout << "fine here pt 2" << endl;
+        return check_knight(i, j, board, attacked_king);
+    } else if (piece == q) {
+        //cout << "fine here pt 3" << endl;
+        return check(i, j, board, 2, attacked_king);
+    } else if (piece == r) {
+        //cout << "fine here pt 4" << endl;
+        return check(i, j, board, 0, attacked_king);
+    } else if (piece == b) {
+        //cout << "fine here pt 5" << endl;
+        return check(i, j, board, 1, attacked_king);
+    } else {
+        return false;
     }
 }
 
-bool expose_white_king(vector<vector<char>> board) {
-    bool white_king_checked = false;
+bool expose_king(vector<vector<char>> board, char king) {
+    char k = king == 'k' ? 'K' : 'k';
+    char q = king == 'k' ? 'Q' : 'q';
+    char n = king == 'k' ? 'N' : 'n';
+    char b = king == 'k' ? 'B' : 'b';
+    char r = king == 'k' ? 'R' : 'r';
     // Traverse entire chess board
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
+            //
             if (board[i][j] == '#') continue;
+            
             // Check if white king is attacked by using the above function
-            if (board[i][j] == 'k' && is_white_king_attacked(i, j, 'k', board)) {
+            //cout << "NOT EVEN HERE?!" << " i: " << i << " j: " << j << endl;
+            if (board[i][j] == k && is_king_attacked(i, j, k, board)) {
+                //cout << "1" << endl;
                 return true;
-            } else if (board[i][j] == 'q' && is_white_king_attacked(i, j, 'q', board)) {
+            } else if (board[i][j] == q && is_king_attacked(i, j, q, board)) { //cout << "2" << endl;
                 return true;
-            } else if (board[i][j] == 'n' && is_white_king_attacked(i, j, 'n', board)) {
+            } else if (board[i][j] == n && is_king_attacked(i, j, n, board)) { //cout << "3" << endl;
                 return true;
-            } else if (board[i][j] == 'b' && is_white_king_attacked(i, j, 'b', board)) {
+            } else if (board[i][j] == b && is_king_attacked(i, j, b, board)) { //cout << "4" << endl;
                 return true;
-            } else if (board[i][j] == 'r' && is_white_king_attacked(i, j, 'r', board)) {
+            } else if (board[i][j] == r && is_king_attacked(i, j, r, board)) { //cout << "5" << endl;
                 return true;
             } else {
                 continue;
@@ -114,17 +142,46 @@ bool valid_move(int c, vector<vector<char>> board) {
     }
     board[1][c] = '#';
     board[0][c] = 'P';
-    return !expose_white_king(board);
+    return !expose_king(board, 'K');
 }
 
 int waysToGiveACheck(vector < vector<char> > board) {
     for (int c = 0; c < 8; c++) {
         if (valid_move(c, board)) {
+            //cout << "HERE!" << endl;
             //Do something...
             board[1][c] = '#';
             board[0][c] = 'P';
+            // If black king is available to be checked, then no matter
+            // what is promoted, it will always be a checked situation
+            if (expose_king(board, 'k')) {
+                return 4;
+            }
+            int count = 0;
+            // Promote to queen
+            if (check(0, c, board, 2, 'k')) {
+                //cout << "Promoted to Queen" << endl;
+                count++;
+            }
+            // Promote to rook
+            if (check(0, c, board, 0, 'k')) {
+                //cout << "Promoted to Rook" << endl;
+                count++;
+            }
+            //Promote to bishop
+            if (check(0, c, board, 1, 'k')) {
+                //cout << "Promoted to Bishop" << endl;
+                count++;
+            }
+            // Promote to knight
+            if (check_knight(0, c, board, 'k')) {
+                //cout << "Promoted to Knight" << endl;
+                count++;
+            }
+            return count;
         }
     }
+    return 0;
 }
 
 int main() {
